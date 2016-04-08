ad7c6f041d1dda122e0ac2f9f2e5453cbfa44936
Augustus Wynn <wyna@seas.upenn.edu>
2016-04-08 14:29:59 -0400

    basic apt-get catches, relies on bash unique 127 error code, test.sh is for testing read -e

--- dispatcher.sh
+++ dispatcher.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-
-
-
 
 while true; do
-  read line
+  echo -n "Bash Dispatcher:"
+  echo -n "$USER:"
+  echo -n $PWD
+  echo "$ "
+  read -e line
   eval $line
+  if [ $? -eq 127 ]; then
+    echo "Command not found, trying to apt-get it, you will need to confirm sudo"
+    sudo apt-get install $(echo $line | awk '{print $1;}')
+ fi
 done
